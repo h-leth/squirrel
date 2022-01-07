ec51aa694edfd478bc8e75ccfeb6a87e6a2e9208
Henning Leth <leth.henning@gmail.com>
2022-01-08 00:23:16 +0100

    speed comparison between the two modules, get_files() is the fastest

--- tmp/get_files.py
+++ tmp/get_files.py
@@ -1,25 +1,29 @@
 import subprocess
 import os
-import glob
 
+file_types = ['*.txt', '*.py']
 
-#def get_files(path, file_types):
-#    find_output = subprocess.run(
-#        f'find {path} -type f -name {}-not -path "*/[@.]*"',
-#        shell=True,
-#        capture_output=True,
-#        text=True
-#    )
-#    return find_output.stdout.strip().split('\n')
+def find_files(path, file_types):
+    project_files = ''
+    for i in file_types:
+        s = f'-name "{i}" -o '
+        project_files += s
+    find_output = subprocess.run(
+        f'find {path} -type f \( {project_files[:-3]} \) -not -path "*/[@.]*"',
+        shell=True,
+        capture_output=True,
+        text=True
+    )
+    return find_output.stdout.strip().split('\n')
 
-
-file_types = ('.txt',)
 
 def get_files(path, file_types):
+    files_tuple = tuple(i[1:] for i in file_types)
     project_files = []
     for root, dirs, files in os.walk(path):
         for filename in files:
-            if filename.endswith(file_types) and not filename.startswith('.'):
+            if filename.endswith(files_tuple) and not filename.startswith('.'):
                 project_files.append(os.path.join(root, filename))
     return project_files
     
+# get_files() is faster than find_files()
